98b4757d2107ff93c094bba9fe764945346f0c8d
dungvuquoc0102 <dung.vuquoc0102@gmail.com>
2026-02-04 17:05:29 +0700

    Add send mail

--- queue.js
+++ queue.js
@@ -1,4 +1,5 @@
 const mysql2 = require("mysql2/promise");
+const nodemailer = require("nodemailer");
 
 (async () => {
   const connection = await mysql2.createConnection({
@@ -19,6 +20,24 @@ const mysql2 = require("mysql2/promise");
     );
 
     console.log("Success");
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "dung.vuquoc0102@gmail.com",
+        pass: "zeoa tfra rfie jgfr", // The 16-character App Password
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: '"Maddison Foo Koch" <maddison53@ethereal.email>',
+      to: "dungvqf8185@fullstack.edu.vn",
+      subject: "Hello ✔",
+      text: "Hello world?", // Plain-text version of the message
+      html: "<b>Hello world?</b>", // HTML version of the message
+    });
+
+    console.log("Message sent:", info.messageId);
 
     await new Promise((resolve) => setTimeout(resolve, 5000));
   }
